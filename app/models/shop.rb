eba3e26aab998fa73df858d0b95f2d98e5b98bb9
EC2 Default User <ec2-user@ip-172-31-15-78.ap-northeast-1.compute.internal>
2024-03-02 15:38:53 +0000

    Commit

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -7,6 +7,8 @@ class Shop < ApplicationRecord
   has_many :revues, :dependent => :destroy
   has_many :revued_users, through: :revues, source: :user
 
+
+
 def revued_by?(user)
     revues.exists?(user_id: user.id)
 end
